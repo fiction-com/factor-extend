93304de45d6bfa6fcbfad789c4de91bc6b2bca9e
Russell <chpmnrssll@gmail.com>
2019-08-26 18:27:49 -0600

    Example app errors on createUser?

--- @plugins/commentize/index.js
+++ @plugins/commentize/index.js
@@ -1,5 +1,18 @@
 export default Factor => {
   return new (class {
-    constructor() {}
+    constructor() {
+      Factor.$filters.add("post-types", _ => {
+        _.push({
+          postType: this.postType,
+          nameIndex: "Comments",
+          nameSingle: "Comment",
+          namePlural: "Comments",
+          // listTemplate: () => import("./dashboard-list.vue"),
+          add: false
+        })
+
+        return _
+      })
+    }
   })()
 }
