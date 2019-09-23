9c0a21c3926738c68a5f0be51e7606450b908f82
Russell <chpmnrssll@gmail.com>
2019-09-23 00:14:51 -0600

    Added cli setup

--- @plugins/commentizer/index.js
+++ @plugins/commentizer/index.js
@@ -1,6 +1,8 @@
 export default Factor => {
   return new(class {
     constructor() {
+      this.addSetupCli()
+
       // Add dashboard component
       Factor.$setting.get("commentizer.postTypes").forEach(postType => {
         Factor.$filters.add("post-edit-components", components => {
@@ -27,11 +29,44 @@ export default Factor => {
       return await Factor.$post.save({
         postType: "commentizer",
         post: {
-          name: commentData.name || "",
+          content: commentData.content || "",
           email: commentData.email || "",
-          content: commentData.content || ""
+          name: commentData.name || ""
         }
       })
     }
+
+    addSetupCli() {
+      // CLI admin setup utility
+      Factor.$filters.add("cli-add-setup", _ => {
+        const setupCommentizer = {
+          name: "Plugin: Commentizer",
+          value: "commentizer",
+          callback: async ({ program, inquirer }) => {
+            let questions = [{
+                name: "postTypes",
+                message: "Select post types to enable comments on.",
+                type: "checkbox"
+              }
+            ]
+
+            const factorPostTypes = await Factor.$post.getPostTypes()
+            factorPostTypes.forEach(postType => {
+              questions.choices.push({ name: postType, value: postType })
+            })
+
+            const { postTypes } = await inquirer.prompt(questions)
+            await Factor.$setup.writeConfig("factor-config", {
+              commentizer: {
+                postTypes
+              }
+            })
+          }
+        }
+
+        return [..._, setupCommentizer]
+      })
+    }
+
   })()
 }
